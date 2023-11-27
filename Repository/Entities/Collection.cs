146bba2e7022636366a3de99db10b68c12a2d685
TamarWalden <t0527149764@gmail.com>
2023-11-27 21:30:36 +0200

    Add project files.

---
+++ Repository/Entities/Collection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+    public class Collection
+    {
+        public int CollectionSymbolization { get; set; }
+        public string Title { get; set; }
+        public string ItemId { get; set; }
+
+    }
+}
